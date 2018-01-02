8dd7f70f83ccdcb468f4f6336b605c28c91206c8
Janice Shiu <contrepoint21@gmail.com>
2018-01-02 20:29:17 +0800

    add initial second selector

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
   $("#first-selector").change(function() {
+    resetFirstSelector(this.value);
   });
+
+  function resetFirstSelector(selectedVal) {
+    hideSecondSelectorChildren();
+    $("#second-selector #" + selectedVal ).show();
+    $("#code").html(" # Create an array<br>numbers = [1, 2, 3] #=> [1, 2, 3]")
+  }
+
+  function hideSecondSelectorChildren() {
+    $("#second-selector").children().hide();
+  }
 });
